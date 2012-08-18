a721b1745e4b3b27b969d8cc77abb803a94d99b9
stingray@hotmail.co.jp <stingray@hotmail.co.jp>
2012-08-18 16:47:13 +0000

    テストを追加したけど、ちょっとなんとかしたい
    git-tfs-id: [https://nnasaki.tfspreview.com/DefaultCollection/]$/tddbc_sendai02;C14

--- tddbc_sendai02/tddbc_sendai02.Tests/VenderMachineTest.cs
+++ tddbc_sendai02/tddbc_sendai02.Tests/VenderMachineTest.cs
@@ -14,5 +14,27 @@ namespace tddbc_sendai02.Tests
             vm.Insert(10);
             Assert.AreEqual(10, vm.AmountOfMoney);
         }
+
+        [TestMethod]
+        public void 五十円玉と百円玉と五百円玉と千円札が1枚ずつ投入できること()
+        {
+            VenderMachineController vm = new VenderMachineController();
+            vm.Insert(50);
+            Assert.AreEqual(50, vm.AmountOfMoney);
+
+            vm = new VenderMachineController();
+            vm.Insert(100);
+            Assert.AreEqual(100, vm.AmountOfMoney);
+
+            vm = new VenderMachineController();
+            vm.Insert(500);
+            Assert.AreEqual(500, vm.AmountOfMoney);
+
+            vm = new VenderMachineController();
+            vm.Insert(1000);
+            Assert.AreEqual(1000, vm.AmountOfMoney);
+        }
+
+
     }
 }
